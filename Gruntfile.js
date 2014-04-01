56578d030023ac75de29dcfa4912f9e067a82dfa
Vern Wolfley <vwolfley@users.noreply.github.com>
2014-03-31 22:09:54 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,9 @@ module.exports = function(grunt){
         pkg: grunt.file.readJSON('package.json')
     });
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['jshint', 'less']);
+    grunt.registerTask('test', ['jshint', 'less']);
+    grunt.registerTask('build', ['jshint', 'uglify', 'less']);
+    grunt.registerTask('production', ['jshint', 'uglify', 'less']);
 
 };
