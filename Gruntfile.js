0f591bd51b56a97cf7587ede41aaf6072e9f3c5d
Vern Wolfley <vwolfley@users.noreply.github.com>
2014-04-08 20:57:58 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,24 +11,15 @@ module.exports = function(grunt){
         htmlhint: {
             build: {
                 options: {
-                     'tag-pair': true,                      
-// Force tags to have a closing pair
-                    'tagname-lowercase': true,             
-// Force tags to be lowercase
-                    'attr-lowercase': true,                
-// Force attribute names to be lowercase e.g. <div id="header"> is invalid
-                    'attr-value-double-quotes': true,      
-// Force attributes to have double quotes rather than single
-                    'doctype-first': true,                 
-// Force the DOCTYPE declaration to come first in the document
-                    'spec-char-escape': true,              
-// Force special characters to be escaped
-                    'id-unique': true,                     
-// Prevent using the same ID multiple times in a document
-                    // 'head-script-disabled': true,          
-// Prevent script tags being loaded in the  for performance reasons
-                    'style-disabled': true                 
-// Prevent style tags. CSS should be loaded through 
+                    'tag-pair': true,                      // Force tags to have a closing pair
+                    'tagname-lowercase': true,             // Force tags to be lowercase
+                    'attr-lowercase': true,                // Force attribute names to be lowercase e.g. <div id="header"> is invalid
+                    'attr-value-double-quotes': true,      // Force attributes to have double quotes rather than single
+                    'doctype-first': true,                 // Force the DOCTYPE declaration to come first in the document
+                    'spec-char-escape': true,              // Force special characters to be escaped
+                    'id-unique': true,                     // Prevent using the same ID multiple times in a document
+                    'head-script-disabled': true,          // Prevent script tags being loaded in the  for performance reasons
+                    'style-disabled': true                 // Prevent style tags. CSS should be loaded through 
                 },
                 src: ['index.html']
             }
